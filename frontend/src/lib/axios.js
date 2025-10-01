0b88269d07a95acb6597627917dddd7a88612a80
Afthar17 <aftharafthar5@gmail.com>
2025-10-01 13:56:18 +0530

    build edit

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
-  // import.meta.mode === "development" ? "http://localhost:5000/api" : "/api",
+  baseURL:
+    import.meta.mode === "development" ? "http://localhost:5000/api" : "/api",
 
   withCredentials: true, //send cookie with request
 });
 
 export default axiosInstance;
+// baseURL: "http://localhost:5000/api",
